0bf68189cb93ef2ca3e7747464dd4694bd2303f5
Stepan <nikiforovstepan2008@gmail.com>
2026-09-23 22:36:39 +0300

    Initial commit: Hello World на C++ и Python

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<string>
+
+int main() {
+            std::cout << "Hello, world!" << std::endl;
+            std::string name = "Никифоров Степан";
+            std::cout<<"Студент:"<<name<<std::endl;
+            std::cout<<"Дата: 2026-09-10"<<std::endl;
+            return 0;
+}
+
---
+++ main.py
@@ -0,0 +1,4 @@
+print("Hello, world!")
+name = "Никифоров Степан"
+print(f"Студент:{name}")
+print("Дата: 2026-09-10")
