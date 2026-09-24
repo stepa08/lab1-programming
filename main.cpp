f70ca03f5c9faa01c3bbf42de213d811f115ac73
Stepan <nikiforovstepan2008@gmail.com>
2026-09-24 13:04:53 +0300

    Форматирование main.cpp через clang-format

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include<string>
+#include <string>
 
 int main() {
-            std::cout << "Hello, world!" << std::endl;
-            std::string name = "Никифоров Степан";
-            std::cout<<"Студент:"<<name<<std::endl;
-            std::cout<<"Дата: 2026-09-10"<<std::endl;
-            return 0;
+    std::cout << "Hello, World!" << std::endl;
+    std::string name = "Никифоров Степан";
+    std::cout << "Студент:" << name << std::endl;
+    std::cout << "Дата: 2026-09-10" << std::endl;
+    return 0;
 }
-
