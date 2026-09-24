7a2576472ff47485f82045c595af26a82f94b8e0
Stepan <nikiforovstepan2008@gmail.com>
2026-09-24 14:17:12 +0300

    Функция square на python

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print("Hello, world!")
-name = "Никифоров Степан"
-print(f"Студент:{name}")
-print("Дата: 2026-09-10")
+# Функция вычисления квадрата числа
+def square(x: int) -> int:
+    return x * x
+
+
+# Динамическая типизация
+number = 7
+# Вызов функции
+result = square(number)
+
+print(f"Число:{number}")
+print(f"Квадрат:{result}")
